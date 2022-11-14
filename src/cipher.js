585e0a93e317cab2766d5ed2740c4546acc39751
unknown <taniafe98@gmail.com>
2022-11-14 09:09:06 -0600

    manejo del DOM con función encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,28 @@
 const cipher = {
-  // ...
-};
+  //Esto servirá para cifrar un mensaje
+  encode(text, offset) {
+    let btncifrar = [];
+  console.log(text, offset)
 
+ for(let i=0; i<text.length; i++) {
+    console.log(i)
+    let codeAscii = text.charCodeAt(i); //formula de codigo ASCII
+    console.log(codeAscii)
+    let textCode = codeAscii + offset
+    console.log(textCode)
+    if(codeAscii > 0){
+      btncifrar.push(String.fromCharCode(textCode));
+     // btncifrar.push(String.fromCharCode(codeAscii));
+      }
+    }
+    return btncifrar.join("")
+
+     console.log(btncifrar) 
+      //let number = (codeAscii - 65 + offset) % 26 + 65; //generar desplazamiento
+     //console.log(number);
+     //let text = String.fromCharCode(number); //Pasa el numero de desplazamiento a la letra mostrada
+     console.log(text);
+     
+}
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+let desplazamiento = document.getElementById("box1");
+let mensaje = document.getElementById("box2");
+let btncifrar = document.getElementById("btn2");
+let decodificar = document.getElementById("btn3");
+let msjcifrado = document.getElementById("box3")
+//let mensaje = document.getElementById ("mensaje").value= btncifrar(cajadetexto, desplazamiento);
+
+let offset;
+
+console.log("afuera" + offset)
+btncifrar.addEventListener("click", function () {
+
+    let text = mensaje.value;
+    offset = parseInt(desplazamiento.value);
+    const encodeText = cipher.encode(text, offset);
+    console.log(encodeText)
+    console.log(text, offset);
+    msjcifrado.value = encodeText;
+    mensaje.value = "";
+
+})
+
+decodificar.addEventListener("click", function (){ 
+    
+})
